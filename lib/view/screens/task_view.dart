b76b1aeda9535a27b11698621bf9a928607cd9b9
mridul <mridulvij1569@gmail.com>
2024-03-20 14:33:17 +0530

    new screen added

--- lib/view/screens/task_view.dart
+++ lib/view/screens/task_view.dart
@@ -1,10 +1,33 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class TaskView extends StatelessWidget {
   const TaskView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: const Column(
+          children: [
+            Expanded(
+              child: Text(
+                'Title',
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+            Divider(),
+            Expanded(
+              child: Text(
+                'Description',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
